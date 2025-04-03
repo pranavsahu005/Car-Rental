e11194e328f946f8eb1b83d0f2e8a85fcce4873e
Pranav Sahu <105783037+pranavsahu005@users.noreply.github.com>
2025-04-03 18:52:39 +0530

    Update CustomerService.py

--- Service/CustomerService.py
+++ Service/CustomerService.py
@@ -6,3 +6,10 @@ class CustomerService:
         self.__Customers.append(Customer)
     def  displayCustomers(self):
         return self.__Customers
+    def removeCustomer(self,index):
+        self.__Customers.pop(index)
+    def searchCustomer(self,Name):
+        for item in self.__Customers:
+            if (Name == item.getName):
+                return item
+        return None
